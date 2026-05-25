324fd6b4e2733dfe46deccb9295ed72dc59c8b02
김민규 <gimmingyu@192.168.nate.com>
2026-05-25 10:40:09 +0900

    Feat : BFS 최단거리

---
+++ src/main/java/org/example/DFS_BFS/P69.java
@@ -0,0 +1,46 @@
+package org.example.DFS_BFS;
+
+import org.example.stackqueue.Problem;
+
+import java.util.Scanner;
+
+public class P69 implements Problem {
+    static int n,m,answer=0;
+    static int[][]graph;
+    static int[]ch;
+    public void DFS(int v){
+        if(v==n)answer++;
+        else{
+            for(int i=1; i<=n; i++){
+                if(graph[v][i]==1 && ch[i]==0){//v정점에서 i 정점까지 이동 갈 수 있다면 1
+                    ch[i]=1;
+                    DFS(i);
+                    ch[i]=0; // 재귀 될 때 백트래킹
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void run() {
+        Scanner kb = new Scanner(System.in);
+        n=kb.nextInt(); // 노드 개수
+        m=kb.nextInt();// 간석 개수
+        graph=new int[n+1][n+1]; // 인접 행렬 1->2 정점번호를 나타냄
+        ch=new int[n+1];
+        for(int i=0; i<m; i++){
+            int a=kb.nextInt();
+            int b=kb.nextInt();
+            graph[a][b]=1;
+        }
+        ch[1]=1; //1번 노드 체크
+        DFS(1);
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) {
+        new P69().run();
+    }
+
+}
---
+++ src/main/java/org/example/DFS_BFS/P70.java
@@ -0,0 +1,67 @@
+package org.example.DFS_BFS;
+
+import org.example.stackqueue.Problem;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class P70 implements Problem {
+    static int n,m;
+    static ArrayList<ArrayList<Integer>>graph;
+    static int[]ch,dis;
+    public void BFS(int v){
+        Queue<Integer> queue=new LinkedList<>();
+        ch[v]=1;// 체크 배열
+        dis[v]=0; // 시작점
+        queue.offer(v);
+        while(!queue.isEmpty()){
+            int cv=queue.poll();
+            for(int nv:graph.get(cv)){ //nv는 cv로 갈 수 있는 정점 graph(cv,[nv...])
+                if(ch[nv]==0){
+                    ch[nv]=1;
+                    queue.offer(nv);
+                    dis[nv]=dis[cv]+1; //nv는 cv를 타고 들어감
+                }
+            }
+
+        }
+
+
+    }
+
+
+
+    @Override
+    public void run() {
+        Scanner kb = new Scanner(System.in);
+        n=kb.nextInt();
+        m=kb.nextInt();
+        graph=new ArrayList<ArrayList<Integer>>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        ch=new int[n+1];
+        dis=new int[n+1];
+        for(int i=0; i<m; i++) {
+           int a=kb.nextInt();
+           int b=kb.nextInt();
+           graph.get(a).add(b);
+        }
+        BFS(1);
+        for(int i=2; i<=n; i++){
+            System.out.println(i+" "+dis[i]);
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new P70().run();
+    }
+
+}
