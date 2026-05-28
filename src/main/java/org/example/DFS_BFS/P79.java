e273ba5b719931c864474cd844052f0041eb3bfe
김민규 <gimmingyu@gimmingyuui-MacBookAir-2.local>
2026-05-28 18:39:07 +0900

    Feat : 메모이제이션 적용

---
+++ src/main/java/org/example/DFS_BFS/P79.java
@@ -0,0 +1,30 @@
+package org.example.DFS_BFS;
+
+import org.example.stackqueue.Problem;
+
+import java.util.Scanner;
+
+//메모이제이션
+public class P79 implements Problem {
+
+    int[][]dy=new int[35][35];
+    public int DFS(int n,int r){
+        if(dy[n][r]>0)return dy[n][r]; // 굳이 뻣지 않게끔
+        if(n==r || r==0)return 1;
+        else return dy[n][r]=DFS(n-1,r-1)+DFS(n-1,r-2);
+    }
+
+    @Override
+    public void run() {
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int r=kb.nextInt();
+        System.out.println(DFS(n,r));
+
+    }
+
+    public static void main(String[] args) {
+        new P79().run();
+    }
+
+}
