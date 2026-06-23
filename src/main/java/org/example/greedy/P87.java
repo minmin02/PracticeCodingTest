f9e6a289fac9bd040be4ede8e65560f748b94161
minmin02 <aktr378@gmail.com>
2026-06-23 14:26:51 +0900

    Feat : 그리디 : 씨름 선수

---
+++ src/main/java/org/example/greedy/P87.java
@@ -0,0 +1,55 @@
+package org.example.greedy;
+
+import org.example.stackqueue.Problem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Body implements Comparable<Body>{
+    public int h,w;
+    Body(int h,int w){
+        this.h=h;
+        this.w=w;
+    }
+
+    @Override
+    public int compareTo(Body o)  {
+        return o.h-this.h; // sort 할 때 ㅗh가 큰 순서대로
+    }
+}
+
+public class P87 implements Problem {
+
+    public int solution(ArrayList<Body> arr,int n) {
+        int cnt=0;
+        Collections.sort(arr);
+        int max=Integer.MIN_VALUE;
+        for(Body ob: arr){
+            if(ob.w>max){
+                max=ob.w;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    @Override
+    public void run() {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        ArrayList<Body> arr=new ArrayList<Body>();
+        for(int i=0; i<n; i++){
+            int h=kb.nextInt();
+            int w=kb.nextInt();
+            arr.add(new Body(h,w));
+        }
+        System.out.println(solution(arr,n));
+    }
+
+    public static void main(String[] args) {
+        new P87().run();
+    }
+
+}
